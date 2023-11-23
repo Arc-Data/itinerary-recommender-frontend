60e90192b37db52348838ae1bb4b7f1fc9fd7436
Emmanuel0725 <emmanuelfrancismacaya@gmail.com>
2023-11-23 17:36:00 +0800

    user bookmark

---
+++ src/components/UserBookmarks.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+const UserBookmarks = (props) => {
+    const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL
+
+    return (
+        <div className="itinerary">
+            <Link to={`/location/${props.details.id}`}>
+                <p className="bookmark--name1 heading4 font-weight-500">
+                    {props.details.name}
+                </p>
+                <div className="itinerary--image-container ">
+                    <img 
+                        src={`${backendUrl}${props.details.primary_image}`} 
+                        height={240}
+                        width={270}
+                        className="itinerary--image"
+                        alt="" />
+                </div>
+                <div>
+                    <div className="bookmark--details">
+                        <p>Opening Time: 
+                            <p 
+                            className="bookmark-item font14 font-weight-500 ">
+                            {props.details.schedule.opening}
+                            </p>
+                        </p>
+                        <p>Closing Time: 
+                            <p 
+                            className="bookmark-item font14 font-weight-500 ">
+                            {props.details.schedule.closing}
+                            </p>
+                        </p>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default UserBookmarks
---
+++ src/pages/Bookmarks.jsx
@@ -0,0 +1,62 @@
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import UserBookmarks from "../components/UserBookmarks";
+
+const Bookmarks = () => {
+    const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL
+    const { authTokens , loading } = useContext(AuthContext);
+    const [usersBookmarks, setUsersBookmarks] = useState([]);
+
+    // GET RECENT BOOKMARKS
+    const getUserBookmarks = async () => {
+        console.log("Fetching Bookmarks");
+        try {
+            const response = await fetch(
+                `${backendUrl}/api/bookmarks/`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${authTokens.access}`,
+                    },
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Error fetching recent bookmarks data");
+            }
+
+            const data = await response.json();
+            console.log(data);
+            setUsersBookmarks(data);
+        } catch (error) {
+            console.error("Error while fetching recent bookmarks data: ", error);
+        }
+    };
+
+    useEffect(() => {
+        getUserBookmarks();
+    }, []);
+
+    const recentBookmarkCards = usersBookmarks && usersBookmarks.map(bookmark => (
+        <UserBookmarks key={bookmark.id} {...bookmark} />
+    ));
+
+    return (
+        <div className="profile--main-content">
+            <p className="header-title heading">Your bookmarks</p>
+            <p className="header-subtitle heading5">Locations</p>
+            {loading ? 
+			<div>
+				Loading...
+			</div>
+			:
+			<div className="trips--container">
+				{recentBookmarkCards}
+			</div>
+			}
+        </div>
+    );
+};
+
+export default Bookmarks;
