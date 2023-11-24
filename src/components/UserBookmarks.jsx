d1f85badc4d61dff98ecf33323c8064e09b6bbbd
Emmanuel0725 <emmanuelfrancismacaya@gmail.com>
2023-11-25 05:28:25 +0800

    nice

--- src/components/UserBookmarks.jsx
+++ src/components/UserBookmarks.jsx
@@ -1,7 +1,4 @@
 import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 
 const UserBookmarks = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL
@@ -9,9 +6,6 @@ const UserBookmarks = (props) => {
     return (
         <div className="itinerary">
             <Link to={`/location/${props.details.id}`}>
-                <p className="bookmark--name1 heading4 font-weight-500">
-                    {props.details.name}
-                </p>
                 <div className="itinerary--image-container ">
                     <img 
                         src={`${backendUrl}${props.details.primary_image}`} 
@@ -20,23 +14,12 @@ const UserBookmarks = (props) => {
                         className="itinerary--image"
                         alt="" />
                 </div>
-                <div>
-                    <div className="bookmark--details">
-                        <p>Minimum Fee 
-                            <p 
-                            className="bookmark-item font14 font-weight-500 ">
-                            {props.details.fee.min}
-                            </p>
-                        </p>
-                        <p>Maximum Fee
-                            <p 
-                            className="bookmark-item font14 font-weight-500 ">
-                            {props.details.fee.max}
-                            </p>
-                        </p>
-                    </div>
-                </div>
             </Link>
+            <div>
+                <p className="bookmark--name1 font-weight-500">
+                    {props.details.name}
+                </p> 
+            </div>
         </div>
     )
 }
