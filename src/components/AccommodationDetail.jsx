c1a4eab922d30d791237c90c86a878e2b72a2717
Emmanuel0725 <emmanuelfrancismacaya@gmail.com>
2023-11-24 18:27:55 +0800

    fixing accomodation details & css

--- src/components/AccommodationDetail.jsx
+++ src/components/AccommodationDetail.jsx
@@ -1,61 +1,75 @@
-import React, { useContext, useEffect, useState } from 'react'
-import AuthContext from '../context/AuthContext'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import { useParams } from "react-router-dom";
 
 const AccommodationDetail = () => {
-	const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL
+  const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 
-	const { authTokens } = useContext(AuthContext)
-	const [services, setServices] = useState([])
-	const { id } = useParams()
+  const { authTokens } = useContext(AuthContext);
+  const [services, setServices] = useState([]);
+  const { id } = useParams();
+  const [showMore, setShowMore] = useState(false);
 
-	const displayServices = services && services.map(service => {
-		return (
-			<div className='food--item' key={service.id}>
-				<div>
-					<img src="" alt="" />
-					<span></span>
-				</div>
-			</div>
-		)
-	})
-	
-	const getServices = async () => {
-		// description : "Experience the serene twilight ambiance by the seaside in our 21 sqm room. The room features one single bed, an anti-bacterial inverter air conditioner, full HD cable TV, telephone, hair dryer, personal fridge, and complimentary coffee/tea service."
-		// id : 1
-		// image: "/media/location_service/OCHSingle.jpg"
-		// item: "Deluxe Single"
-		// price: 1700
-		try {
-			const response = await fetch(`${backendUrl}/api/service/${id}/`, {
-				"method": "GET",
-				"headers": {
-					"Content-Type": "application/json",
-					"Authorization": `Bearer ${authTokens.access}`
-				}
-			})
-			const data = await response.json()
-			setServices(data)
-		}
-		catch (error) {
-			console.log("Error occured while fetching services")
-		}
-	}
+  const displayServices = services.map((service) => (
+    <div className="service--item" key={service.id}>
+      <div>
+        <img
+          className="searchPage--pic"
+          src={`${backendUrl}${service.image}`}
+          alt={service.item}
+        />
+        <span className="font-weight-600">{service.item}</span>
+        <div
+          className="service--descriptions"
+          style={{ maxHeight: showMore ? "none" : "50px" }}
+        >
+          <span className="service--description font13">
+            {service.description}
+          </span>
+        </div>
+        {service.description.length > 50 && (
+          <button
+            className="show-more-button"
+            onClick={() => setShowMore(!showMore)}
+          >
+            {showMore ? "Show Less" : "Show More"}
+          </button>
+        )}
+      </div>
+      <div className="service--price--container">
+        <div className="description--item-price">₱{service.price}</div>
+      </div>
+    </div>
+  ));
 
-	useEffect(() => {
-		getServices()
-	}, [])
-	
-	return (
-		<div className='food-place--container'>
-			<div className='food-place--header'>
-				<p className="heading2">Services</p>
-			</div>
-			<div className='food-items'>
-				{displayServices}
-			</div>
-		</div>
-	)
-}
+  const getServices = async () => {
+    try {
+      const response = await fetch(`${backendUrl}/api/service/${id}/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authTokens.access}`,
+        },
+      });
+      const data = await response.json();
+      setServices(data);
+    } catch (error) {
+      console.log("Error occurred while fetching services");
+    }
+  };
 
-export default AccommodationDetail
+  useEffect(() => {
+    getServices();
+  }, []);
+
+  return (
+    <div className="food-place--container">
+      <div className="food-place--header">
+        <p className="heading2">Services</p>
+      </div>
+      <div className="food-items">{displayServices}</div>
+    </div>
+  );
+};
+
+export default AccommodationDetail;
