5b116227c406a73bf10bf00f778fd0d96d81782d
Arc <arclight04@gmail.com>
2023-11-27 02:10:13 +0800

    services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,13 +5,52 @@ import useBusinessManager from "../hooks/useBusinessManager"
 
 
 const Services = () => {
+    const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL
+
     const { id } = useParams()
     const { authTokens } = useContext(AuthContext)
-    const { items:foods, loading, error, getFoodItems } = useBusinessManager(authTokens) 
+    const { items, location, loading, error, getFoodItems, getBusinessDetail, handleServiceType } = useBusinessManager(authTokens) 
+
+    console.log(items)
+
+    const displayFood = items && items.map(item => {
+        return (
+            <tr key={item.id}>
+                <td>
+                    <img src={`${backendUrl}${item.image}`} alt="" />
+                </td>
+                <td>{item.item}</td>
+                <td>{item.price}</td>
+            </tr>
+        )
+    })
+
+    const displayServices = items && items.map(item => {
+        return (
+            <tr key={item.id}>
+                <td>Temp data</td>
+            </tr>
+        )
+    })
+
+
+    const displayItems = () => {
+        if (location.location_type === "2") {
+            return displayFood
+        } else if (location.location_type === "3") {
+            return displayServices
+        }
+    }
 
     useEffect(() => {
-        getFoodItems(id)
-    }, [])
+        getBusinessDetail(id)
+    }, [id])
+
+    useEffect(() => {
+        if (location) {
+            handleServiceType(location.id, location.location_type)
+        }
+    }, [location])
 
     if (loading) {
         return (
@@ -25,10 +64,27 @@ const Services = () => {
         )
     }
 
-    console.log(foods)
-
     return (
-        <div>Services</div>
+        <div>
+            <p>Services</p>
+            <div>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <table className='business--app-table'>
+                <thead>
+                    <tr>
+                        <th>Business name</th>
+                        <th>Date filed</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {displayItems()}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
