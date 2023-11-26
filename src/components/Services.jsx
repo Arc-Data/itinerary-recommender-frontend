884f685dbd2cf04cc185f99e071e095b7ed16a2a
Arc <arclight04@gmail.com>
2023-11-27 01:13:08 +0800

    get food items

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,35 @@
-const Serivces = () => {
+import { useContext, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import AuthContext from "../context/AuthContext"
+import useBusinessManager from "../hooks/useBusinessManager"
+
+
+const Services = () => {
+    const { id } = useParams()
+    const { authTokens } = useContext(AuthContext)
+    const { items:foods, loading, error, getFoodItems } = useBusinessManager(authTokens) 
+
+    useEffect(() => {
+        getFoodItems(id)
+    }, [])
+
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div>{error}</div>
+        )
+    }
+
+    console.log(foods)
+
     return (
-        <div>Serivces</div>
+        <div>Services</div>
     )
 }
 
-export default Serivces
+export default Services
