218aae1ab5c021542cb297d32fec913982efc217
Arc <arclight04@gmail.com>
2023-11-27 19:29:34 +0800

    fixed food place and accommodation places not rendering

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -27,7 +27,6 @@ import {
 	faClock,
 	faMap,
 	faBookmark,
-	faTags
 } from '@fortawesome/free-solid-svg-icons';
 
 export default function DetailPage() {
@@ -185,11 +184,6 @@ export default function DetailPage() {
 		return (<div>Please wait</div>)
 	}
 
-	const displayActivities = location && location.details.activities.map((activity, index) => {
-		return (
-			<div key={index} className="detailPage--tag">{activity}</div>
-		)
-	})
 
 	const editReview = async () => {
 		try {
@@ -323,25 +317,6 @@ export default function DetailPage() {
 		}
 	};
 
-	const displayRequiredFees = location && location.details.required_fee.map((fee, index) => {
-		return (
-			<div key={index} className="detailPage--fee span-items">
-				<p className="font-weight-600">{fee.fee_type}({fee.audience_type})</p>
-				<p>{fee.price}</p>
-			</div>
-		)
-	}) 
-
-	const displayOptionalFees = location && location.details.optional_fee.map((fee, index) => {
-		return (
-			<div key={index} className="detailPage--fee span-items">
-				<p className="font-weight-600">{fee.fee_type}({fee.audience_type})</p>
-				<p>{fee.price}</p>
-			</div>
-		)
-	}) 
-
-
 	const resultStart = (currentPage - 1) * 5 + 1;
 	const resultEnd = Math.min(currentPage * 5, reviewData.length);
 
@@ -399,29 +374,10 @@ export default function DetailPage() {
 				</div>
 			</div>
 		</div>
-		<div className="detailPage--additional-details">
-			<div className="detailPage--activities">
-				<p className="heading2"><FontAwesomeIcon className="btn-icons" icon={faTags} />Activities</p>
-				<div className="detailPage--tags">
-					{displayActivities}
-				</div>
-			</div>
-			<div className="detailPage--required-fees">
-				<p className="heading2"><FontAwesomeIcon className="btn-icons" icon={faMoneyBills} />Required fees</p>
-				<div className="detailPage--fees-details">
-					{displayRequiredFees}
-				</div>
-			</div>
-			<div className="detailPage--optional-fees">
-				<p className="heading2"><FontAwesomeIcon className="btn-icons" icon={faMoneyBills} />Optional fees</p>
-				<div className="detailPage--fees-details">
-					{displayOptionalFees}
-				</div>
-			</div>
-		</div>
+		
 		
 		{location?.location_type === "1" && 
-		<SpotDetail />
+		<SpotDetail location={location}/>
 		}
 		{location?.location_type === "2" &&
 		<FoodDetail isOwnedByUser={location.owner === user.user_id}/>
