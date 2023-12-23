896c116d003e2dc1485e4684af03cffb876c0b25
Arc <arclight04@gmail.com>
2023-12-23 15:19:44 +0800

    added key to location item

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -111,7 +111,7 @@ const Day = ({ day, updateDays, removeDay, addMarker, deleteMarker, includedLoca
 
     const itineraryLocations = () => locations.map(location => {
         return (
-            <LocationItem location={location} />
+            <LocationItem location={location} key={location.id}/>
         )
     })
 
