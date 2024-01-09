caf1e1445e39dbacbb3d2336612c27e261824e3b
Arc <arclight04@gmail.com>
2024-01-09 10:15:16 +0800

    added spinner for registration

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,9 +3,10 @@ import { Link, Navigate } from 'react-router-dom';
 import AuthContext from "../context/AuthContext"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import Spinner from "../components/Spinner";
 
 const Signup = () => {
-    const { registerUser, user } = useContext(AuthContext)
+    const { registerUser, user, status, setStatus } = useContext(AuthContext)
     const [passwordMatch, setPasswordMatch] = useState(false)
     const [formData, setFormData] = useState({
         'firstname': '',
@@ -15,6 +16,7 @@ const Signup = () => {
         'contact': '',
         'confirm': '',
     })
+    const [ loading, setLoading ] = useState(false)
     const [showPassword, setShowPassword] = useState(false)
     
     if(user) {
@@ -35,12 +37,15 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-
+        setLoading(true)
+        
         if(passwordMatch) {
-            registerUser(formData)
+            await registerUser(formData)
         } else {
             alert("Passwords do not match")
         }
+
+        setLoading(false)
     }
 
     const togglePasswordVisibility = () => {
@@ -113,7 +118,11 @@ const Signup = () => {
                     onClick={togglePasswordVisibility}>
                     {showPassword ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
                 </button>
+                {loading ? 
+                <Spinner /> 
+                :
                 <button className='button-login-sign' type="submit">Sign Up</button>
+                }
                 <div className="login-sign-link">
                     Already have an account? <Link to="/login">Login</Link>
                 </div>
