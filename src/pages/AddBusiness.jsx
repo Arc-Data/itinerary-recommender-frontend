64ded9c0b5027ebe957896e1a008988826c7509c
smyleeee <141607668+smyleeee@users.noreply.github.com>
2024-01-14 22:36:50 +0800

    nice

--- src/pages/AddBusiness.jsx
+++ src/pages/AddBusiness.jsx
@@ -508,7 +508,6 @@ const AddBusiness = () => {
                                                 onKeyDown={ yDown}
                                                 placeholder="Add or search tags (e.g. Filipino, Fastfood)"
                                                 className="tags-input"
-                                                required
                                             />
                                         </div>
                                         {tagSearchResults}
@@ -530,7 +529,7 @@ const AddBusiness = () => {
                                                             onKeyDown={handleKeyDown}
                                                             placeholder="Add or search activities (e.g., Sightseeing, Swimming)"
                                                             className="tags-input"
-                                                            required
+
                                                         />
                                                     </div>
                                                     {tagSearchResults}
@@ -545,7 +544,7 @@ const AddBusiness = () => {
                                                             checked={tags.includes(tag.name)}
                                                             onChange={(e) => handleSpotTagChange(e, tag.name)}
                                                             className="tags-checkbox"
-                                                            required
+
                                                         />
                                                         <label className="tags-checkbox-label" htmlFor={`tag-${index}`}>{tag.name}</label>
                                                     </div>
